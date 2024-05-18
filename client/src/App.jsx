49bd3cb1b6947b787e3527d00f9d242b0da23dc7
santiago daniel <santi-dy240@hotmail.com>
2024-05-18 15:38:03 -0600

    add class loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -52,10 +52,13 @@ const router = createBrowserRouter([
 ])
 const override = {
   position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width:"100px",
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 };
 function App() {
 
