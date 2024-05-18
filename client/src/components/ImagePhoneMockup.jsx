724fcc07f8344573870139c4ce81338a478d18ca
santiago daniel <santi-dy240@hotmail.com>
2024-05-18 14:22:52 -0600

    url image phone

--- client/src/components/ImagePhoneMockup.jsx
+++ client/src/components/ImagePhoneMockup.jsx
@@ -1,7 +1,10 @@
+
+const URL_BACK=import.meta.env.VITE_BACK_URL||"http://localhost:3002"
+
 function ImagePhoneMockup({user}) {
     return (
         <image
-            href={`http://localhost:3002/${user?.photo ?? ""}`} x="105.5" y="64" height="96" width="96"
+            href={`${URL_BACK}/${user?.photo ?? ""}`} x="105.5" y="64" height="96" width="96"
             clip-path="url(#circleView)"
         />
     )
