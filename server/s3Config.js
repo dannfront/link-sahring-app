92ec23257fb016ae74591110361925d782d6fc62
santiago daniel <santi-dy240@hotmail.com>
2024-05-22 18:03:46 -0600

    Remove AWS credentials

--- server/s3Config.js
+++ server/s3Config.js
@@ -1,6 +1,6 @@
 import { S3Client } from "@aws-sdk/client-s3";
-// import dotenv from "dotenv";
-// dotenv.config({path:'./config.env'})
+import dotenv from "dotenv";
+dotenv.config({path:'./config.env'})
 // config s3 client
 const client=new S3Client({
     region:process.env.REGION_AWS,
